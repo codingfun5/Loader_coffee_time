b5a1aee17a34ec1ac926b92f040412a0250787b6
Sofya <sarksofya@yandex.ru>
2024-01-13 21:25:42 +0300

    new file:   .vscode/settings.json 	modified:   src/App.css 	modified:   src/App.js 	new file:   src/Loader/Loader.js 	new file:   src/Loader/LoaderPage.js 	new file:   src/Loader/style.css

---
+++ src/Loader/Loader.js
@@ -0,0 +1,17 @@
+import './style.css';
+const Loader = () => {
+    return(
+        <div className='container'>
+      
+ <div class="cup">
+ <div>
+ <h1>Coffee time</h1>
+ </div>      
+	<div class="handle"></div>
+</div>
+
+</div>
+      
+    )
+}
+export default Loader;
---
+++ src/Loader/LoaderPage.js
@@ -0,0 +1,9 @@
+import Loader from "./Loader";
+const LoaderPage = () => {
+    return(
+        <div className="container_loader">
+<Loader/>
+        </div>
+    )
+}
+export default LoaderPage;
